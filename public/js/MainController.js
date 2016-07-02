301aa2d84bf0d5f9c6b53b9b7393e04780168c8a
HardHatArea <jon.hoopes@gmail.com>
2016-07-02 12:43:35 -0600

    end of class on Saturday

--- public/js/MainController.js
+++ public/js/MainController.js
@@ -1,5 +1,44 @@
 angular.module('Trello')
 
-.controller('MainController', function ($scope, $http) {
-  
+.controller('MainController', function ($scope, dataService) {
+
+  // $http.get('/api/hello-world')
+  // .then(function(response) {
+  //   console.log(response)
+  // })
+
+  // var newTask = {
+  //   text: 'Do laundry.'
+  // }
+  //
+  // $http.post('/api/task', newTask)
+  // .then(function(response) {
+  //   console.log(response);
+  // });
+
+  dataService.getAllData()
+    .then(function(response) {
+      $scope.lists = response.data;
+      console.log(response);
+    })
+    .catch(function(err) {
+      console.error(err);
+    })
+
+    $scope.addTask = function(listIndex, newTask) {
+      dataService.addTask(listIndex, newTask)
+      .then(function(response) {
+        console.warn(response)
+        $scope.lists[listIndex].tasks.push(response.data);
+      })
+      .catch(function(err) {
+        console.error(err);
+      })
+    }
+
+  // $scope.addTask = function() {
+  //   dataService.getData();
+  // }
+
+
 });
--- server.js
+++ server.js
@@ -1,1 +1,50 @@
+var express     = require('express'),
+    app         = express(),
+    bodyParser  = require('body-parser');
 
+app.use(express.static('public'));
+
+//middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+//to fake a database
+var lists = [{
+  name: 'personal', tasks: ['sleep', 'eat']
+}, {
+  name: 'school', tasks: ['homework', 'study for the test']
+}, {
+  name: 'work', tasks: ['get rich', 'get that promotion', 'slap the CEO in the face']
+}];
+
+// app.get('/api/hello-world', function(req, res) {
+//   res.send('hello worldly');
+// })
+
+// app.post('/api/task', function(req, res) {
+//   console.log(req.body);
+//   res.send(req.body);
+// })
+
+app.get('/api/data', function(req, res) {
+  // console.log(lists);
+  res.send(lists);
+})
+
+app.put('/api/list/:listIndex', function(req, res) {
+  var listIndex = req.params.listIndex;
+  // console.log(lists[listIndex], req.body);
+  lists[listIndex].tasks.push(req.body.text);
+  // console.log(lists);
+  res.send(req.body.text);
+})
+
+// app.get('/', function (req, res) {
+//   res.send('hellow world!'));
+// });
+
+
+var portNum = 3000
+app.listen(portNum, function() {
+  console.log('listening via port:', portNum)
+});
